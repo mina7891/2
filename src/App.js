4d4e3c943be686b0bf9e4108273bae2f2e61b56e
mina7891 <zlwhs813@naver.com>
2025-04-08 13:51:46 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div style={{ fontFamily: 'Pretendard, sans-serif', padding: '2rem' }}>
+      <h1 style={{ color: '#ff69b4' }}>Welcome to MINA MARKET!</h1>
+      <p>This is your baby & kids shoes store, built with ❤️.</p>
+    </div>
+  );
+}
+export default App;
