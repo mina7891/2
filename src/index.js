e5072170612c8bb6c31aa82aad4118fa2b852881
mina7891 <zlwhs813@naver.com>
2025-04-08 14:48:16 +0900

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import MinaMarketPreview from "./MinaMarketUI";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <MinaMarketPreview />
+  </React.StrictMode>
+);
